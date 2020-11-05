9852b8532f45c40f3eb36b4fc21f24a353b9d8ac
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-05 11:52:59 -0300

    Update 1.Ubuntu-Setup.sh

--- 1.Ubuntu-Setup.sh
+++ 1.Ubuntu-Setup.sh
@@ -4,19 +4,19 @@
 
 # System Update and Upgrade 
 sudo apt update && 
-sudo apt upgrade -yy &&
-sudo apt dist-upgrade -yy &&
-sudo apt install -f -yy &&
-sudo apt autoremove -yy &&
+sudo apt upgrade -y &&
+sudo apt dist-upgrade -y &&
+sudo apt install -f &&
+sudo apt autoremove -y &&
 sudo apt autoclean &&
 sudo apt clean &&
 
 # Flatpak Support
-sudo apt install -yy flatpak &&
-sudo apt install -yy gnome-software-plugin-flatpak &&
+sudo apt install -y flatpak &&
+sudo apt install -y gnome-software-plugin-flatpak &&
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # End of Script
 
 # Display Installation Complete Message
-echo "Great! Please, restart the computer now and then run the 2nd Script to install all your Apps."
+echo "All good now! Please, restart the computer and then run the 2nd Script to install all your Apps."
