8d2918ed497efd76a4eb81812f576c3db65925c8
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-08 16:22:45 -0300

    Update 1.Ubuntu-Setup.sh

--- 1.Ubuntu-Setup.sh
+++ 1.Ubuntu-Setup.sh
@@ -5,25 +5,25 @@
 # Uninstall Bloatware Apps
 sudo apt --purge remove -yy yelp gnome-logs seahorse &&
 
-# System Update and Upgrade 
+# System Update and Upgrade
 sudo apt update && 
-sudo apt upgrade -y &&
-sudo apt dist-upgrade -y &&
+sudo apt upgrade -yy &&
+sudo apt dist-upgrade -yy &&
 
 # System Backup
-sudo apt-add-repository ppa:teejee2008/ppa -y &&
+sudo apt-add-repository ppa:teejee2008/ppa -yy &&
 sudo apt update &&
-sudo apt install -y timeshift &&
+sudo apt install -yy timeshift &&
 
 # System Clean Up
 sudo apt install -f &&
-sudo apt autoremove -y &&
+sudo apt autoremove -yy &&
 sudo apt autoclean &&
 sudo apt clean &&
 
 # Flatpak Support
-sudo apt install -y flatpak &&
-sudo apt install -y gnome-software-plugin-flatpak &&
+sudo apt install -yy flatpak &&
+sudo apt install -yy gnome-software-plugin-flatpak &&
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # End of Script
