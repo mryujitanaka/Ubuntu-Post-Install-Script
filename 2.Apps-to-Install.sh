bf0f310fd3ab3fa519248bedb93813453627f2b2
Yuji Tanaka <yujitanaka.developer@gmail.com>
2020-11-28 09:10:01 -0300

    Update 2.Apps-to-Install.sh

--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -3,91 +3,91 @@
 ## Apps to Install ##
 
 # System Update
-sudo apt update &&
+sudo apt update
 
 # Ubuntu Software App Store
-sudo apt install -yy gnome-tweaks gparted synaptic gufw hardinfo dconf-editor baobab autokey-gtk virtualbox neofetch htop net-tools ffmpeg clamav clamav-daemon libclamunrar9 &&
+sudo apt install -y gnome-tweaks gparted synaptic gufw hardinfo dconf-editor baobab autokey-gtk virtualbox neofetch htop net-tools ffmpeg clamav clamav-daemon libclamunrar9
 
 # PPA's Apps from Launchpad
 ## Adding Papirus Icons
-sudo add-apt-repository ppa:papirus/papirus -yy
+sudo add-apt-repository ppa:papirus/papirus -y
 ## Adding Grub Customizer
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -yy
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 ## Adding qBittorrent
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -yy
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 ## Adding Git
-sudo add-apt-repository ppa:git-core/ppa -yy
+sudo add-apt-repository ppa:git-core/ppa -y
 ## Adding Cryptomator
-sudo add-apt-repository ppa:sebastian-stenzel/cryptomator -yy
+sudo add-apt-repository ppa:sebastian-stenzel/cryptomator -y
 ## Adding KeePassXC
-sudo add-apt-repository ppa:phoerious/keepassxc -yy
+sudo add-apt-repository ppa:phoerious/keepassxc -y
 ## Adding OBS Studio
-sudo add-apt-repository ppa:obsproject/obs-studio -yy
+sudo add-apt-repository ppa:obsproject/obs-studio -y
 ### System Update
-sudo apt update -qq &&
+sudo apt update -qq
 #### Installing the Programs
-sudo apt install -yy papirus-icon-theme grub-customizer qbittorrent git cryptomator keepassxc obs-studio &&
+sudo apt install -y papirus-icon-theme grub-customizer qbittorrent git cryptomator keepassxc obs-studio
 
 # PPA's Apps from Other Sources
 ## Adding Amazon Corretto 11
-wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - &&
-sudo add-apt-repository 'deb https://apt.corretto.aws stable main' &&
+wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
+sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
 ## Adding GitHub Desktop - The Linux Fork
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null &&
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list' &&
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 ## Adding Brave Browser
-sudo apt install apt-transport-https curl gnupg &&
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - &&
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
+sudo apt install apt-transport-https curl gnupg
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 ### System Update
-sudo apt update -qq &&
+sudo apt update -qq
 #### Installing the Programs
-sudo apt install -yy java-common java-11-amazon-corretto-jdk github-desktop brave-browser &&
+sudo apt install -y java-common java-11-amazon-corretto-jdk github-desktop brave-browser
 
 # Snap Apps
 ## IntelliJ IDEA
-sudo snap install intellij-idea-community --classic &&
+sudo snap install intellij-idea-community --classic
 
 # Flatpak Update
 flatpak update -y
 
 # Flatpak Apps
 ## VLC
-flatpak install flathub org.videolan.VLC -yy &&
+flatpak install flathub org.videolan.VLC -y
 ## AnyDesk
-flatpak install flathub com.anydesk.Anydesk -yy &&
+flatpak install flathub com.anydesk.Anydesk -y
 ## Tor Browser
-flatpak install flathub com.github.micahflee.torbrowser-launcher -yy &&
+flatpak install flathub com.github.micahflee.torbrowser-launcher -y
 ## GIMP
-flatpak install flathub org.gimp.GIMP -yy &&
+flatpak install flathub org.gimp.GIMP -y
 ## LibreOffice
-flatpak install flathub org.libreoffice.LibreOffice -yy &&
+flatpak install flathub org.libreoffice.LibreOffice -y
 ## Meld
-flatpak install flathub org.gnome.meld -yy &&
+flatpak install flathub org.gnome.meld -y
 ## Audacity
-flatpak install flathub org.audacityteam.Audacity -yy &&
+flatpak install flathub org.audacityteam.Audacity -y
 ## Visual Studio Code
-flatpak install flathub com.visualstudio.code -yy &&
+flatpak install flathub com.visualstudio.code -y
 
 # Flatpak Clean Up
-flatpak uninstall --delete-data -yy &&
-flatpak uninstall --unused -yy &&
+flatpak uninstall --delete-data -y
+flatpak uninstall --unused -y
 
 # Some Other Settings
 ## Permanently enable Do Not Disturb (No Notifications)
-gsettings set org.gnome.desktop.notifications show-banners false &&
+gsettings set org.gnome.desktop.notifications show-banners false
 ## Enable Hidden Startup Programs
-sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop &&
+sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
 
 # Finishing Things Up
 ## System Update and Upgrade
-sudo apt update && 
-sudo apt upgrade -yy &&
-sudo apt dist-upgrade -yy &&
+sudo apt update 
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 ## System Clean Up
-sudo apt install -f &&
-sudo apt autoremove -yy &&
-sudo apt autoclean &&
+sudo apt install -f
+sudo apt autoremove -y
+sudo apt autoclean
 sudo apt clean
 
 # End of Script
