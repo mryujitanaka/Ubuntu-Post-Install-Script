940f8be72e1e1586eebbe49dc7d20db816418a59
Yuji Tanaka <yujitanaka.developer@gmail.com>
2021-04-30 08:41:55 -0300

    Update Ubuntu-Post-Install-Script

--- 1.Ubuntu-Setup.sh
+++ 1.Ubuntu-Setup.sh
@@ -10,10 +10,11 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 
-# System Backup
-sudo apt-add-repository ppa:teejee2008/ppa -y
-sudo apt update
-sudo apt install -y timeshift
+# Activate the lines below by removing the comments (###) if you want Timeshift to be installed.
+## System Backup
+### sudo apt-add-repository ppa:teejee2008/ppa -y
+### sudo apt update
+### sudo apt install -y timeshift
 
 # System Clean Up
 sudo apt install -f
@@ -21,12 +22,13 @@ sudo apt autoremove -y
 sudo apt autoclean
 sudo apt clean
 
-# Flatpak Support
-sudo apt install -y flatpak
-sudo apt install -y gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Activate the lines below by removing the comments (###) if you want Flatpak to be installed on your PC. Check beforehand if your Operating System already supports program installations via flatpak.
+## Flatpak Support
+### sudo apt install -y flatpak
+### sudo apt install -y gnome-software-plugin-flatpak
+### flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # End of Script
 
 # Display Installation Complete Message
-echo "All good now :) Please, restart the computer, backup your system using Timeshift and then run the 2nd Script to install all your Apps."
+echo "All good now :) Please, restart the computer, backup your system using Timeshift (if you installed it) and then run the 2nd Script to install all your Apps."
--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -11,8 +11,6 @@ sudo apt install -y gnome-tweaks gparted synaptic gufw hardinfo dconf-editor bao
 # PPA's Apps from Launchpad
 ## Adding Papirus Icons
 sudo add-apt-repository ppa:papirus/papirus -y
-## Adding Grub Customizer
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 ## Adding qBittorrent
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 ## Adding Git
@@ -21,29 +19,35 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:sebastian-stenzel/cryptomator -y
 ## Adding KeePassXC
 sudo add-apt-repository ppa:phoerious/keepassxc -y
+# If you want to install OBS Studio and / or Grub Customizer on your PC, disable the comments (####) and add after the line "#### Installing the Programs", obs-studio and grub-customizer to be installed with the other apps.
 ## Adding OBS Studio
-sudo add-apt-repository ppa:obsproject/obs-studio -y
+### sudo add-apt-repository ppa:obsproject/obs-studio -y
+## Adding Grub Customizer
+### sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 ### System Update
 sudo apt update -qq
 #### Installing the Programs
-sudo apt install -y papirus-icon-theme grub-customizer qbittorrent git cryptomator keepassxc obs-studio
+sudo apt install -y papirus-icon-theme qbittorrent git cryptomator keepassxc
 
 # PPA's Apps from Other Sources
 ## Adding Amazon Corretto 11
 wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
-## Adding GitHub Desktop - The Linux Fork
-### wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-### sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 ## Adding Brave Browser
 sudo apt install apt-transport-https curl gnupg
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# The GitHub Desktop PPA is not working to add to the system at this time. Try going to the developer's repository and download the latest version compatible with your OS.
+# If you decide to try to install GitHub Desktop anyway, remove the comments (###) that are in front of the lines that start with wget and sudo, and add github-desktop after the line "#### Installing the Programs".
+## Adding GitHub Desktop - The Linux Fork
+### wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
+### sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 ### System Update
 sudo apt update -qq
 #### Installing the Programs
 sudo apt install -y java-common java-11-amazon-corretto-jdk brave-browser
 
+# If your system is compatible with Snap technology, disable commenting (###).
 # Snap Apps
 ## IntelliJ IDEA
 ### sudo snap install intellij-idea-community --classic
@@ -60,8 +64,6 @@ flatpak install flathub com.anydesk.Anydesk -y
 flatpak install flathub com.github.micahflee.torbrowser-launcher -y
 ## GIMP
 flatpak install flathub org.gimp.GIMP -y
-## LibreOffice
-flatpak install flathub org.libreoffice.LibreOffice -y
 ## Meld
 flatpak install flathub org.gnome.meld -y
 ## Audacity
@@ -93,4 +95,4 @@ sudo apt clean
 # End of Script
 
 # Display Installation Complete Message
-echo "All done :) Please, restart the computer, clean up your system using BleachBit and then restart the computer again."
+echo "All done :) Please, restart the computer, clean up your system using BleachBit (if you want) and then restart the computer again."
