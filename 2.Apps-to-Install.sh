73d2c433884a767630f31b81b10eeec07a9aa308
Yuji Tanaka <yujitanaka.developer@gmail.com>
2023-12-27 14:51:45 -0300

    Update 2.Apps-to-Install.sh

--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -115,7 +115,7 @@ echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-st
 sudo apt update -qq
 ### PPA's Apps from Other Sources installation
 #### sudo apt install -y java-11-amazon-corretto-jdk
-sudo apt install -y nodejs
+#### sudo apt install -y nodejs
 #### sudo apt install -y yarn
 sudo apt install -y brave-browser
 sudo apt install -y anydesk
