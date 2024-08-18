f91910feca7ecc362440af24afdf0a992a340ed3
Yuji Tanaka <yujitanaka.developer@gmail.com>
2024-08-18 18:24:57 -0300

    Update 2.Apps-to-Install.sh

--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -139,6 +139,8 @@ flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.audacityteam.Audacity -y
 ## FileZilla
 flatpak install flathub org.filezillaproject.Filezilla -y
+## Varia
+flatpak install flathub io.github.giantpinkrobots.varia -y
 ## Discord
 ### flatpak install flathub com.discordapp.Discord -y
 ## Zoom
