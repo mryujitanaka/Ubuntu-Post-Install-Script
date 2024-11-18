3add88408d8e5133e4e36485929b272e04ec95f9
Yuji Tanaka <yujitanaka.developer@gmail.com>
2024-11-18 11:36:06 -0300

    Update 2.Apps-to-Install.sh

--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -35,25 +35,17 @@ sudo apt install -y ffmpeg
 sudo apt install -y lightdm-gtk-greeter-settings
 ## To use Java based applications like LibreOffice extensions and/or develop java apps, install the JRE or JDK packages below.
 ## JRE/JDK 17 from OpenJDK
-sudo apt install -y openjdk-17-jdk
-sudo apt install -y openjdk-17-jdk-headless
-sudo apt install -y openjdk-17-jre
-sudo apt install -y openjdk-17-jre-headless
-sudo apt install -y openjdk-17-jre-zero
+### sudo apt install -y openjdk-17-jdk
+### sudo apt install -y openjdk-17-jdk-headless
+### sudo apt install -y openjdk-17-jre
+### sudo apt install -y openjdk-17-jre-headless
+### sudo apt install -y openjdk-17-jre-zero
 ## JRE/JDK 21 from OpenJDK
 ### sudo apt install -y openjdk-21-jdk
 ### sudo apt install -y openjdk-21-jdk-headless
 ### sudo apt install -y openjdk-21-jre
 ### sudo apt install -y openjdk-21-jre-headless
 ### sudo apt install -y openjdk-21-jre-zero
-## JRE/JDK 21 from AdoptOpenJDK Eclipse Temurin
-### sudo apt install -y wget apt-transport-https gpg
-### wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-### echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-### sudo apt update -qq
-### sudo apt install -y temurin-21-jdk
-### sudo apt install -y temurin-21-jre
-## OpenJ9 JVM
 
 ## If your DE is GNOME, then download the packages below.
 ### sudo apt install -y gnome-tweaks
