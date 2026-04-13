8aaa8d99656c93c407b63d99f5f91208dd891053
Yuji Tanaka <yujitanaka.developer@gmail.com>
2026-04-13 16:13:00 -0300

    Update 2.Apps-to-Install.sh

--- 2.Apps-to-Install.sh
+++ 2.Apps-to-Install.sh
@@ -6,9 +6,7 @@
 sudo apt update
 
 # Apps
-sudo apt install -y gparted
 sudo apt install -y synaptic
-sudo apt install -y gufw
 sudo apt install -y baobab
 sudo apt install -y net-tools
 sudo apt install -y ttf-mscorefonts-installer
@@ -156,6 +154,10 @@ sudo rm -rf /var/lib/snapd/cache/*
 # wget Apps
 ## hardinfo2
 wget -c "https://release-assets.githubusercontent.com/github-production-release-asset/754780099/01dbea68-ad28-493a-8f10-7b9bf1b1f278?sp=r&sv=2018-11-09&sr=b&spr=https&se=2026-04-13T15%3A18%3A30Z&rscd=attachment%3B+filename%3Dhardinfo2_2.2.15-Ubuntu-22.04_amd64.deb&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2026-04-13T14%3A18%3A02Z&ske=2026-04-13T15%3A18%3A30Z&sks=b&skv=2018-11-09&sig=R2H631rhCHL00siAPZqqoswZaNJB%2Ff%2F1kH2a9%2BUuCiM%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc3NjA5MDE5OSwibmJmIjoxNzc2MDg5ODk5LCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.9hMXk7y85FqnchZe5550kMpdnpVmw3c9DXNll_HoBh4&response-content-disposition=attachment%3B%20filename%3Dhardinfo2_2.2.15-Ubuntu-22.04_amd64.deb&response-content-type=application%2Foctet-stream"
+## GParted
+wget -c "https://sinalbr.dl.sourceforge.net/project/gparted/gparted/gparted-1.8.1/gparted-1.8.1.tar.gz?viasf=1"
+## gufw
+wget -c "https://release-assets.githubusercontent.com/github-production-release-asset/218600664/eabed39d-f6e1-481a-945d-989253766eab?sp=r&sv=2018-11-09&sr=b&spr=https&se=2026-04-13T16%3A19%3A37Z&rscd=attachment%3B+filename%3Dgui-ufw-26.04.0.tar.gz&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2026-04-13T15%3A19%3A03Z&ske=2026-04-13T16%3A19%3A37Z&sks=b&skv=2018-11-09&sig=rE6fqt0wFT4VylM%2B5FN6bDixUzPN0LWEGyn4g41mqP4%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc3NjA5NDM3NCwibmJmIjoxNzc2MDk0MDc0LCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.RlbHuctoabIWAOEA-Pb1ikpCvNXnP-X4QzbWxy7h3fo&response-content-disposition=attachment%3B%20filename%3Dgui-ufw-26.04.0.tar.gz&response-content-type=application%2Foctet-stream"
 ## BleachBit
 wget -c "https://download.bleachbit.org/bleachbit_5.0.0-0_all_ubuntu2204.deb"
 ## bottom
